ce16d0e7c183960aafa39f0789d0933d24014056
marcellodev <marcellovasconcelos@globomail.com>
2018-11-03 00:53:53 -0200

    foi

---
+++ QRM/site/index.php
@@ -0,0 +1,14 @@
+<? php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="dist/css/bootstrap-grid.min.css" />
+    <title>QRM</title>
+</head>
+<body>
+    
+</body>
+</html>
