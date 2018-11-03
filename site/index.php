c801ce630d4b954393c7d7e4af9888880e80843c
marcellodev <marcellovasoncelos@globomail.com>
2018-11-03 09:41:55 -0200

    login pronto

---
+++ site/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/index.css" />
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Titillium+Web:300,400,600,700" rel="stylesheet">
+    <title>QR Medical</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h1><span>QR</span> Medical</h1>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <section class="login">
+                    <form action="login.php" method="post">
+                        <div>
+                            <input type="email" name="login" id="" placeholder="Email">
+                            <input type="password" name="password" placeholder="Senha">
+                        </div> 
+                        <input type="submit" name="enviar">
+                    </form>
+                </section>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
