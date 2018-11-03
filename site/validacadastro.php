0e7f073e695cb9ca92ff0c7aa4654d32a3475439
marcellodev <marcellovasoncelos@globomail.com>
2018-11-03 21:38:03 -0200

    cadastro

--- site/validacadastro.php
+++ site/validacadastro.php
@@ -1,9 +1,7 @@
 <?php 
 
-$login = $_POST['loginCad'];
 $senha = $_POST['senhaCad'];
 $tipo = $_POST['tipo'];
-$clinica = isset($_POST['clinicaCad']);
 $nome = $_POST['nomeCad'];
 $data = $_POST['dataCad'];
 $sexo = $_POST['sexoCad'];
@@ -21,22 +19,19 @@ $estado = $_POST['estado'];
 $con = mysql_connect("localhost", "root", "") or die ("Sem conexão com o servidor");
 $select = mysql_select_db("qrm") or die("Sem acesso ao DB, Entre em contato com o Administrado");
 
-
-
 $compl = mysql_real_escape_string($compl,$con);
-$resultUsu = mysql_query("INSERT INTO usuario (login, senha, nomeUsuario, email, rua, bairro, cep, complemento, uf, cidade, sexo, idTipoUsuario) VALUES ('$login', '$senha', '$nome', '$email', '$logradouro', '$bairro', '$cep', '$compl', '$estado', '$cidade', '$sexo', '$tipo')") or die (mysql_error()); 
+$resultUsu = mysql_query("INSERT INTO usuario (senha, email, tipo, cep, logradouro, bairro, cidade, estado) VALUES ('$senha', '$email', '$tipo','$cep','$logradouro','$bairro','$cidade','$estado' )") or die (mysql_error()); 
  
-
-	$resultTel1 = mysql_query("INSERT INTO telefone VALUES('$tel',(SELECT idUsuario FROM usuario where login = '$login'))") or die(mysql_error());
+$resultTel1 = mysql_query("INSERT INTO telefone VALUES('$tel',(SELECT idUsuario FROM usuario where login = '$email'))") or die(mysql_error());
 
 if ($tel2) {
-	$tel2 = $_POST['telCad2'];
-	$resultTel2 = mysql_query("INSERT INTO telefone VALUES('$tel2',(SELECT idUsuario FROM usuario where login = '$login'))") or die(mysql_error());
+	$tel2 = $_POST['telRecadoCad2'];
+	$resultTel2 = mysql_query("INSERT INTO telefone VALUES('$tel2',(SELECT idUsuario FROM usuario where login = '$email'))") or die(mysql_error());
 }
 
 session_start();
 
-$_SESSION['login'] = $login;
+$_SESSION['email'] = $email;
 $_SESSION['senha'] = $senha;
 $_SESSION['tipo'] = $tipo;
 
